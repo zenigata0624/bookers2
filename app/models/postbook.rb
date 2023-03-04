bd1e28e4846ddad8e703fe7da92a08c6827ed30a
tenyuikeda <124487739+zenigata0624@users.noreply.github.com>
2023-03-04 08:42:18 +0000

    [start]bookers2

---
+++ app/models/postbook.rb
@@ -0,0 +1,2 @@
+class Postbook < ApplicationRecord
+end
---
+++ db/migrate/20230304081919_create_books.rb
@@ -0,0 +1,10 @@
+class CreateBooks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :books do |t|
+      t.string :book_name
+      t.text :caption
+      t.integer :user_id
+      t.timestamps
+    end
+  end
+end

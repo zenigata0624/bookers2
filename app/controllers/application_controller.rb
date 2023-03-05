6111380dd8a0782a0040bb7fd453379ac0fd49d0
tenyuikeda <124487739+zenigata0624@users.noreply.github.com>
2023-03-05 08:00:49 +0000

    [start]bookers2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,9 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   
   def after_sign_in_path_for(resource)
-    books_path
+      user_path(resource)
   end
   
-   def after_sign_out_path_for(resource)
-    about_path
-   end
-
   
   protected
   
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,19 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update
+    redirect_to user_path(resource)
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name,:profile_image)
+  end
+
 end

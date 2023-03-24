0642fd82a6997487d79ea7c2cce54986794eb53d
tenyuikeda <124487739+zenigata0624@users.noreply.github.com>
2023-03-24 10:23:06 +0000

    [finish]bookers2 非同期通信

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,21 +1,25 @@
 class BookCommentsController < ApplicationController
+ 
   def create
-   book = Book.find(params[:book_id])
-   comment = current_user.book_comments.new(post_comment_params)
-   comment.book_id = book.id
+   @book = Book.find(params[:book_id])
+   comment = current_user.book_comments.new(book_comment_params)
+   comment.book_id = @book.id
    comment.save
-   redirect_to book_path(book)
-  end
-  
+    @books=Book.find(params[:book_id])
+    @book_comment = BookComment.new
+ end
+
   def destroy
-    BookComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+   @book= Book.find(params[:book_id])
+   @book_comment= BookComment.find_by(id: params[:id], book_id: params[:book_id])
+   @book_comment.destroy
+   @book_comment = BookComment.new
   end
-  
+
   private
-  
-  def  post_comment_params
+
+  def  book_comment_params
     params.require(:book_comment).permit(:comment)
   end
-  
+
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,7 @@ import "bootstrap";
 import "../stylesheets/application" 
 import '@fortawesome/fontawesome-free/js/all'
 
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()

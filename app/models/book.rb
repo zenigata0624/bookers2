f6aadf7843fe5efe68a628bd52b260cd7a82db91
tenyuikeda <124487739+zenigata0624@users.noreply.github.com>
2023-04-04 04:35:30 +0000

    [start]bookers2 9aまで

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,7 +3,6 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :book_comments,dependent: :destroy
   has_many :favorites,dependent: :destroy
-  has_many :favorited_users, through: :favorites, source: :user
   has_many :view_counts, dependent: :destroy
   
   validates :body ,presence: true,

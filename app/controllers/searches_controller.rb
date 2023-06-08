efbbfda3e08c80765ef92053a9cdbeccca9e6612
tenyuikeda <124487739+zenigata0624@users.noreply.github.com>
2023-06-08 02:51:01 +0000

    ネストのやり方を学ぶ

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,6 +5,7 @@ class SearchesController < ApplicationController
   def search
     @range = params[:range]
 
+
     if @range == "User"
       @users = User.looks(params[:search], params[:word])
     else
